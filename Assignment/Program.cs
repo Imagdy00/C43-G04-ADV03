7ca0952a148cfd68cc7e8e1d77a56aab81dc05cf
Imagdy00 <125517448+Imagdy00@users.noreply.github.com>
2025-02-07 19:04:21 +0200

    Add project files.

---
+++ Assignment/Program.cs
@@ -0,0 +1,114 @@
+﻿namespace Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Complete the Book Class
+            //    public class Book
+            //{
+            //    public string ISBN { get; set; }
+            //    public string Title { get; set; }
+            //    public string[] Authors { get; set; }
+            //    public DateTime PublicationDate { get; set; }
+            //    public decimal Price { get; set; }
+
+            //    public Book(string _ISBN, string _Title, string[] _Authors, DateTime _PublicationDate, decimal _Price)
+            //    {
+            //        ISBN = _ISBN;
+            //        Title = _Title;
+            //        Authors = _Authors;
+            //        PublicationDate = _PublicationDate;
+            //        Price = _Price;
+            //    }
+
+            //    public override string ToString()
+            //    {
+            //        string authors = string.Join(", ", Authors);
+            //        return $"ISBN: {ISBN}, Title: {Title}, Authors: {authors}, Publication Date: {PublicationDate.ToShortDateString()}, Price: {Price:C}";
+            //    }
+            //}
+            #endregion
+
+
+            #region Complete the BookFunctions Class
+            //    public class BookFunctions
+            //{
+            //    public static string GetTitle(Book B)
+            //    {
+            //        return B.Title;
+            //    }
+
+            //    public static string GetAuthors(Book B)
+            //    {
+            //        return string.Join(", ", B.Authors);
+            //    }
+
+            //    public static string GetPrice(Book B)
+            //    {
+            //        return B.Price.ToString("C");
+            //    }
+            //}
+            #endregion
+
+
+            #region Implement the LibraryEngine Class with Different Delegates
+            //    public class LibraryEngine
+            //{
+
+            //    public delegate string BookFunctionDelegate(Book B);
+
+
+            //    public static void ProcessBooks(List<Book> blist, BookFunctionDelegate fPtr)
+            //    {
+            //        foreach (Book B in blist)
+            //        {
+            //            Console.WriteLine(fPtr(B));
+            //        }
+            //    }
+
+
+            //    public static void ProcessBooks(List<Book> blist, Func<Book, string> fPtr)
+            //    {
+            //        foreach (Book B in blist)
+            //        {
+            //            Console.WriteLine(fPtr(B));
+            //        }
+            //    }
+            //}
+            #endregion
+
+
+
+            #region Demonstrate Usage with Different Cases
+        //    class Program
+        //{
+        //    static void Main(string[] args)
+        //    {
+        //        List<Book> books = new List<Book>
+        //{
+        //    new Book("123-456", "C# Programming", new string[] { "John Doe", "Jane Smith" }, new DateTime(2020, 1, 1), 29.99m),
+        //    new Book("789-012", "Advanced C#", new string[] { "Alice Johnson" }, new DateTime(2021, 5, 15), 49.99m)
+        //};
+
+                
+        //        LibraryEngine.BookFunctionDelegate titleDelegate = BookFunctions.GetTitle;
+        //        LibraryEngine.ProcessBooks(books, titleDelegate);
+
+                
+        //        Func<Book, string> authorsDelegate = BookFunctions.GetAuthors;
+        //        LibraryEngine.ProcessBooks(books, authorsDelegate);
+
+                
+        //        LibraryEngine.BookFunctionDelegate isbnDelegate = delegate (Book B) { return B.ISBN; };
+        //        LibraryEngine.ProcessBooks(books, isbnDelegate);
+
+                
+        //        Func<Book, string> pubDateDelegate = B => B.PublicationDate.ToShortDateString();
+        //        LibraryEngine.ProcessBooks(books, pubDateDelegate);
+        //    }
+        //}
+        #endregion
+    }
+    }
+}
---
+++ Demo/Program.cs
@@ -0,0 +1,210 @@
+﻿using System;
+
+namespace Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Video 1
+            //            using System;
+
+            //            delegate void MyDelegate(string message);
+
+            //class Program
+            //        {
+            //            static void Main()
+            //            {
+            //                MyDelegate del = new MyDelegate(ShowMessage);
+            //                del("Hello, World!");
+            //            }
+
+            //            static void ShowMessage(string message)
+            //            {
+            //                Console.WriteLine(message);
+            //            }
+            //        }
+            #endregion
+
+            #region Video 2
+            //            using System;
+
+            //            delegate int MathOperation(int a, int b);
+
+            //class Program
+            //        {
+            //            static void Main()
+            //            {
+            //                MathOperation add = new MathOperation(Add);
+            //                Console.WriteLine(add(5, 3));
+            //            }
+
+            //            static int Add(int a, int b)
+            //            {
+            //                return a + b;
+            //            }
+            //        }
+            #endregion
+
+
+            #region Video 3
+            //            using System;
+
+            //            delegate void PrintDelegate(string message);
+
+            //class Program
+            //        {
+            //            static void Main()
+            //            {
+            //                PrintDelegate print = new PrintDelegate(PrintMessage);
+            //                print("Hello, Delegate!");
+            //            }
+
+            //            static void PrintMessage(string message)
+            //            {
+            //                Console.WriteLine(message);
+            //            }
+            //        }
+            #endregion
+
+
+            #region Video 4
+            //            using System;
+
+            //            delegate void PrintDelegate<T>(T message);
+
+            //class Program
+            //        {
+            //            static void Main()
+            //            {
+            //                PrintDelegate<string> print = new PrintDelegate<string>(PrintMessage);
+            //                print("Hello, Generic Delegate!");
+            //            }
+
+            //            static void PrintMessage(string message)
+            //            {
+            //                Console.WriteLine(message);
+            //            }
+            //        }
+            #endregion
+
+
+            #region Video 5
+            //            using System;
+
+            //            delegate void Operation(int a, int b);
+
+            //class Program
+            //        {
+            //            static void Main()
+            //            {
+            //                Operation op = Add;
+            //                op += Subtract;
+            //                op(10, 5);
+            //            }
+
+            //            static void Add(int a, int b)
+            //            {
+            //                Console.WriteLine($"Sum: {a + b}");
+            //            }
+
+            //            static void Subtract(int a, int b)
+            //            {
+            //                Console.WriteLine($"Difference: {a - b}");
+            //            }
+            //        }
+            #endregion
+
+
+            #region Video 6
+            //            using System;
+
+            //            delegate void Operation<T>(T a, T b);
+
+            //class Program
+            //        {
+            //            static void Main()
+            //            {
+            //                Operation<int> op = Add;
+            //                op += Subtract;
+            //                op(10, 5);
+            //            }
+
+            //            static void Add(int a, int b)
+            //            {
+            //                Console.WriteLine($"Sum: {a + b}");
+            //            }
+
+            //            static void Subtract(int a, int b)
+            //            {
+            //                Console.WriteLine($"Difference: {a - b}");
+            //            }
+            //        }
+            #endregion
+
+            #region Video 7
+            //            using System;
+
+            //class Program
+            //        {
+            //            static void Main()
+            //            {
+            //                Action<string> print = Console.WriteLine;
+            //                print("Hello, Action!");
+
+            //                Func<int, int, int> add = (a, b) => a + b;
+            //                Console.WriteLine(add(5, 3));
+            //            }
+            //        }
+            #endregion
+
+
+            #region Video 8
+            //            using System;
+
+            //class Program
+            //        {
+            //            static void Main()
+            //            {
+            //                Action<string> print = delegate (string message) {
+            //                    Console.WriteLine(message);
+            //                };
+            //                print("Hello, Anonymous Method!");
+
+            //                Action<string> printLambda = message => Console.WriteLine(message);
+            //                printLambda("Hello, Lambda Expression!");
+            //            }
+            //        }
+            #endregion
+
+            #region Video 9
+            //            using System;
+
+            //class Program
+            //        {
+            //            static void Main()
+            //            {
+            //                var number = 10;
+            //                var message = "Hello, var!";
+            //                Console.WriteLine($"{message} {number}");
+            //            }
+            //        }
+            #endregion
+
+
+            #region Video 10
+////            using System;
+
+////class Program
+////        {
+////            static void Main()
+////            {
+////                Func<int, Func<int, int>> add = x => y => x + y;
+////                var addFive = add(5);
+////                Console.WriteLine(addFive(3)); // Output: 8
+////            }
+////        }
+        #endregion
+    }
+    }
+}
